e2846769c0dd6b20a8e7978bd960bf6e31366cc1
TheAnonymous-stack <thanhnht.2014@gmail.com>
2024-10-26 20:20:25 -0400

    attempt to fix server component

--- app/utils/chatgpt.js
+++ app/utils/chatgpt.js
@@ -3,7 +3,7 @@ import OpenAI from 'openai';
 import fs from 'fs';
 
 const openai = new OpenAI({
-  apiKey: process.env.apiKey
+  apiKey: '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';
 });
 async function getHours(filePaths) {
   console.log(filePaths);
@@ -14,7 +14,6 @@ async function getHours(filePaths) {
     model: "gpt-4o-mini",
     tools: [{ type: 'file_search'}],
     });
-    console.log("Assistant created sucessfully");
 
     const attachment_files = [];
     for (let filePath of filePaths) {
@@ -37,7 +36,6 @@ async function getHours(filePaths) {
         }
       ]
     });
-    console.log("thread created successfully");
     const run = await openai.beta.threads.runs.createAndPoll(thread.id, {
       assistant_id: assistant.id,
     });
@@ -48,7 +46,6 @@ async function getHours(filePaths) {
     const message = messages.data.pop();
     if (message && message.content[0].type === 'text') {
       const { text } = message.content[0];
-      console.log("response received");
       const { annotations } = text;
       const citations = [];
 
